03c7822fd05b3edfae76b993e2aaac5fbd0756ab
Grafrestone <80029985+Grafredstone@users.noreply.github.com>
2025-01-17 16:59:27 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,11 @@
-let Salt = 0
-let Started = 0
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(receivedNumber / Salt)
 })
 radio.onReceivedValue(function (name, value) {
-    Started = 1
     radio.setGroup(216)
     basic.showIcon(IconNames.Yes)
-    basic.showLeds(`
-        . # # . .
-        . # . # .
-        . # . # .
-        . # . # .
-        . # # . .
-        `)
-    Salt = value
     radio.setGroup(parseFloat(name))
+    Salt = value
 })
-basic.forever(function () {
-    while (Started == 0) {
-        basic.showIcon(IconNames.Diamond)
-        basic.pause(200)
-        basic.showIcon(IconNames.SmallDiamond)
-        basic.pause(200)
-    }
-})
+let Salt = 0
+basic.showString("D")
