036855147894ebfd50d101300f6a034d9ae1e881
Grafrestone <80029985+Grafredstone@users.noreply.github.com>
2025-01-17 21:11:08 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber / Salt)
+    Temp = receivedNumber / Salt
+    basic.showNumber(Temp, 100)
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
-    basic.showString("" + Salt + "|" + Funkgruppe)
+    basic.showString("" + Salt + "|" + Funkgruppe, 150)
 })
 radio.onReceivedValue(function (name, value) {
-    radio.setGroup(216)
-    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.Yes, 500)
+    basic.clearScreen()
     Funkgruppe = parseFloat(name)
     radio.setGroup(Funkgruppe)
     Salt = value
 })
 let Funkgruppe = 0
 let Salt = 0
+let Temp = 0
+radio.setGroup(216)
 basic.showString("D")
